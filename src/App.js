0174f9d0fe9907cb05e5cb1cc66d7d6a4982a204
Ashutosh9599 <ashutoshdas091@gmail.com>
2023-12-18 15:34:10 +0530

    Card and filestructure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
   const expenses = [
@@ -7,26 +7,27 @@ function App() {
     { id: 3, date: new Date(2023, 12, 18), expenseName: 'Movies', amount: 'Rs 200', locationOfExpenditure: 'Cinema' },
   ];
 
-  const expenseItems = [];
-  for (let i = 0; i < expenses.length; i++) {
-    const expense = expenses[i];
-    expenseItems.push(
-      <ExpenseItem
-        key={expense.id}
-        date={expense.date}
-        expenseName={expense.expenseName}
-        amount={expense.amount}
-        locationOfExpenditure={expense.locationOfExpenditure}
-      />
-    );
-  }
+  // const expenseItems = [];
+  // for (let i = 0; i < expenses.length; i++) {
+  //   const expense = expenses[i];
+  //   expenseItems.push(
+  //     <ExpenseItem
+  //       key={expense.id}
+  //       date={expense.date}
+  //       expenseName={expense.expenseName}
+  //       amount={expense.amount}
+  //       locationOfExpenditure={expense.locationOfExpenditure}
+  //     />
+  //   );
+  // }
 
   return (
     <div className="expense-list">
-      <h2>Expense Items</h2>
+      {/* <h2>Expense Items</h2>
       <div>
         {expenseItems}
-      </div>
+      </div> */}
+      <Expenses items={expenses} />
     </div>
   );
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,18 @@
+import React from 'react';
+
 import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
 
   return (
-    <div className="expense-list-item">
+    <Card className="expense-list-item">
       <ExpenseDate date={props.date} />
       <h2>{props.expenseName}</h2>
       <div className="expense-location">{props.locationOfExpenditure}</div>
       <div className="expense-list-item__price">{props.amount}</div>
-    </div>
+    </Card>
   );
 }
 
